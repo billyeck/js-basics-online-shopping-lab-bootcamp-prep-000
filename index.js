8a17c81a708fd56c83fbbc1d8939e576d1337862
William Eck <billyeck@gmail.com>
2019-04-18 15:26:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,7 +40,19 @@ function total() {
 }
 
 function removeFromCart(item) {
-  
+  var inCart=0;
+  for (var i=0; i<cart.length; i++){
+    if (cart[i].itemName===item){
+      delete cart[i];
+      inCart=1;
+    }
+  }
+  if (inCart===0){
+    return "That item is not in your cart";
+  }
+  if (inCart===1){
+    return cart;
+  }
 }
 
 function placeOrder(cardNumber) {
