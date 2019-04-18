b9266c8b42ebe366654943228efa106fb0538b51
William Eck <billyeck@gmail.com>
2019-04-18 15:45:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if Number.isNan(cardNumber){
+  if (Number.isNan(cardNumber)){
     return "Sorry, we don't have a credit card on file for you."
   }
 }
