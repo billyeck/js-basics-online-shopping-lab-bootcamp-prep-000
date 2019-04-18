d8f8728553e832a498763d47ab065756f9349b49
William Eck <billyeck@gmail.com>
2019-04-18 16:06:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,9 +56,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  currentTotal=total();
+  cart=[];
   if (isNaN(cardNumber)){
     return "Sorry, we don't have a credit card on file for you.";
   }
-  else return "Your total cost is $"+total()+", which will be charged to the card "+cardNumber+".";
-  cart=[];
+  else return "Your total cost is $"+currentTotal", which will be charged to the card "+cardNumber+".";
 }
